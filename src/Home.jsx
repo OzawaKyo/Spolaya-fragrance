10a264c463673e2e2e41a445be98731f3ac81719
Mohamed Amarcha <simoamarcha@icloud.com>
2023-03-01 23:21:18 +0100

    homepage responsive

--- src/Home.jsx
+++ src/Home.jsx
@@ -5,13 +5,15 @@ export default function Home(){
         <div className="Home">
             <img className="black" src={black} alt="" />
             <div className="home-text">
-                <div>
+                <div className="part1">
                     <h1 className="smell">Take</h1>
                     <h1 className="smell">care of</h1>
                     <h1 className="smell">your smell</h1>
                 </div>
-                <p className="desc">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus, enim aspernatur. Corporis praesentium tenetur atque aliquid eius dolore similique cumque corrupti autem, facilis non aspernatur consectetur tempora quidem dignissimos hic.</p>
-                <button className="button-27 shop-now">SHOP NOW</button>
+                <div className="part2">
+                    <p className="desc">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus, enim aspernatur. Corporis praesentium tenetur atque aliquid eius dolore similique cumque corrupti autem, facilis non aspernatur consectetur tempora quidem dignissimos hic.</p>
+                    <button className="button-27 shop-now">SHOP NOW</button>
+                </div>
             </div>
         </div>
     )
